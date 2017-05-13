9835516b7c2de99a1a16bf94e601b2049e4db44a
Benjamin TOLLET <btollet@e1r12p9.42.fr>
2017-05-13 17:04:44 +0200

    Form

--- app.js
+++ app.js
@@ -5,50 +5,62 @@ var upload = multer();
 var app = express();
 var bdd;
 
+let register = require('./include/register');
+
 MongoClient.connect('mongodb://localhost:27017', (err, base) => {
-  if (err) throw err;
-  bdd = base;
+    if (err) throw err;
+    bdd = base;
 });
 
 app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res) => {
-  let test = 'Coucou';
-  res.render('pages/index', { test: test});
+    let test = 'Coucou';
+    res.render('pages/index', { test: test});
 });
 
 app.get('/register', (req, res) => {
-  res.render('pages/register');
+    res.render('pages/register');
 });
 
 app.post('/register', upload.fields([]), (req, res) => {
-  console.log(req.body);
-  if (req.body.login != null && req.body.pass != '')
-  {
-    bdd.collection('users').insertOne({"login": req.body.login, "pass": req.body.pass}, (err) => {
-      if (err) return(console.log(err));
-      console.log('Bdd add');
-    });
+    register.check_login(req.body.login, bdd, (val) => check_login = val);              // Login
+    register.check_mail(req.body.mail, bdd, (val) => check_mail = val);                // Mail
+    let check_pass = register.check_pass(req.body.pass, req.body.pass_confirm, bdd);    //Pass
+
+    if (check_login == 'ok' && check_pass == 'ok' && check_mail == 'ok')
+    {
+        bdd.collection('users').insertOne({"login": req.body.login, "pass": req.body.pass, "mail": req.body.mail}, (err) => {
+            if (err) return(console.log(err));
+            console.log('Bdd add');
+        });
+    }
+    else {
+        console.log('----------------------------');
+        console.log('Login => ' + check_login);
+        console.log('Pass => ' + check_pass);
+        console.log('Mail => ' + check_mail);
+    }
     res.send('Ok');
-  }
-  else res.send('Error');
+
 });
 
+
 app.get('/show', (req, res) => {
-  let cursor = bdd.collection('users').find();
-  cursor.each((err, val) => {
-    if (val) console.log(val);
-  });
-  console.log('');
-  res.send('Ok');
+    let cursor = bdd.collection('users').find();
+    cursor.each((err, val) => {
+        if (val) console.log(val);
+    });
+    console.log('');
+    res.send('Ok');
 });
 
 app.get('/del', (req, res) => {
-  bdd.collection('users').deleteMany();
-  res.send('Delete');
+    bdd.collection('users').deleteMany();
+    res.send('Delete');
 });
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000');
+    console.log('Listening on port 3000');
 });
---
+++ include/register.js
@@ -0,0 +1,52 @@
+module.exports = {
+    check_login: (login, bdd, callback) => {
+        if (login.length >= 3 && login.length <= 10)
+        {
+            find_user (login, bdd, (val) => callback (val));
+        }
+        else
+        callback ('len');
+    },
+
+    check_pass: (pass, pass_confirm, bdd) => {
+        if (pass == pass_confirm)
+        {
+            if (pass.length >= 8)
+            {
+                return ('ok');
+            }
+            else
+            return ('len');
+        }
+        else
+        return ('diff');
+    },
+
+    check_mail: (mail, bdd, callback) => {
+        if (mail != '')
+        {
+            find_mail (mail, bdd, (val) => callback (val));
+        }
+        else
+        callback ('len');
+    }
+};
+
+function find_user(login, bdd, callback) {
+    bdd.collection('users').find({login: login}).toArray((err, result) => {
+        console.log('in function'); 
+        if (result[0])
+            callback('exist');
+        else
+            callback('ok');
+    });
+}
+
+function find_mail(mail, bdd, callback) {
+    bdd.collection('users').find({mail: mail}).toArray((err, result) => {
+        if (result[0])
+            callback('exist');
+        else
+            callback('ok');
+    });
+}
