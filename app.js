c70da74cfcc116ddad3b1879e11c0e978d44f353
Benjamin <benjamin@MacBook-Pro-de-Benjamin.local>
2017-05-14 16:41:45 +0200

    Register async + login check

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var multer = require('multer');
 var MongoClient = require('mongodb').MongoClient;
+var bcrypt = require('bcrypt');
 var upload = multer();
 var app = express();
 var bdd;
@@ -25,44 +26,11 @@ app.get('/register', (req, res) => {
 });
 
 app.post('/register', upload.fields([]), (req, res) => {
-    register.check_login(req.body.login, bdd, (val) => {
-        check_login = val;              // Login
-        register.check_mail(req.body.mail, bdd, (val) => {
-            check_mail = val;                // Mail
-            let check_pass = register.check_pass(req.body.pass, req.body.pass_confirm, bdd);    //Pass
-
-            if (check_login == 'ok' && check_pass == 'ok' && check_mail == 'ok')
-            {
-                bdd.collection('users').insertOne({"login": req.body.login, "pass": req.body.pass, "mail": req.body.mail}, (err) => {
-                    if (err) return(console.log(err));
-                    console.log('Bdd add');
-                });
-            }
-            else {
-                console.log('----------------------------');
-                console.log('Login => ' + check_login);
-                console.log('Pass => ' + check_pass);
-                console.log('Mail => ' + check_mail);
-            }
-        });
-    });
-    res.send('Ok');
-
+    register.check_user(req.body, bdd, res);
 });
 
-
-app.get('/show', (req, res) => {
-    let cursor = bdd.collection('users').find();
-    cursor.each((err, val) => {
-        if (val) console.log(val);
-    });
-    console.log('');
-    res.send('Ok');
-});
-
-app.get('/del', (req, res) => {
-    bdd.collection('users').deleteMany();
-    res.send('Delete');
+app.post('/login_check', upload.fields([]), (req, res) => {
+    register.check_login(req.body.login, bdd, res);
 });
 
 app.listen(3000, () => {
--- include/register.js
+++ include/register.js
@@ -1,51 +1,61 @@
 module.exports = {
-    check_login: (login, bdd, callback) => {
-        if (login.length >= 3 && login.length <= 10)
+    check_user: (user, bdd, res) => {
+        if (user.login.length >= 3 && user.login.length <= 10)
         {
-            find_user (login, bdd, (val) => callback (val));
-        }
-        else
-        callback ('len');
-    },
-
-    check_pass: (pass, pass_confirm, bdd) => {
-        if (pass == pass_confirm)
-        {
-            if (pass.length >= 8)
-            {
-                return ('ok');
-            }
+            if (check_pass(user.pass, user.pass_confirm) == 'ok' && check_mail(user.mail) == 'ok')
+            find_user(user, bdd, res);
             else
-            return ('len');
+            res.end('Password ou mail err');
         }
         else
-        return ('diff');
+        res.end('Login len');
     },
 
-    check_mail: (mail, bdd, callback) => {
-        if (mail != '')
-        {
-            find_mail (mail, bdd, (val) => callback (val));
-        }
-        else
-        callback ('len');
+    check_login: (login, bdd, res) => {
+        login_only(login, bdd, res);
     }
 };
 
-function find_user(login, bdd, callback) {
-    bdd.collection('users').find({login: login}).toArray((err, result) => {
-        if (result[0])
-            callback('exist');
-        else
-            callback('ok');
-    });
+async function find_user(user, bdd, res) {
+    let count = await bdd.collection('users').find({$or: [
+        {login: user.login},
+        {mail: user.mail}
+    ]}).count();
+    if (count == 0) {
+        bdd.collection('users').insertOne({"login": user.login, "pass": user.pass, "mail": user.mail}, (err) => {
+            if (err) return(console.log(err));
+            console.log('Bdd add');
+        });
+        res.end('ok');
+    }
+    else
+    res.end('login Existe');
 }
 
-function find_mail(mail, bdd, callback) {
-    bdd.collection('users').find({mail: mail}).toArray((err, result) => {
-        if (result[0])
-            callback('exist');
+async function login_only(login, bdd, res) {
+    let count = await bdd.collection('users').find({login: login}).count();
+    if (count == 0)
+    res.end('ok');
+    else
+    res.end('exist');
+}
+
+function check_pass (pass, pass_confirm) {
+    if (pass == pass_confirm)
+    {
+        if (pass.length >= 8)
+        {
+            return ('ok');
+        }
         else
-            callback('ok');
-    });
+        return ('len');
+    }
+    else
+    return ('diff');
+}
+
+function check_mail (mail) {
+    if (mail.length > 0)
+    return('ok');
+    return('error');
 }
