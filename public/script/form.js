209f85ceab727bc19da78e118e0a8eb30458d2a1
Benjamin TOLLET <btollet@e1r12p9.42.fr>
2017-05-16 17:36:51 +0200

    Form

--- public/script/form.js
+++ public/script/form.js
@@ -1,12 +1,12 @@
 function sucess(form, div, err, err_mes) {
-    form.setAttribute('class', 'form-control form-control-success');
+    form.setAttribute('class', 'form-control form-control-success mb-2 mr-sm-2 mb-sm-0');
     div.setAttribute('class', 'form-group has-success');
     err.setAttribute('hidden', 'hidden');
     err_mes.setAttribute('hidden', 'hidden');
 }
 
 function danger(form, div, err, err_mes) {
-    form.setAttribute('class', 'form-control form-control-danger');
+    form.setAttribute('class', 'form-control form-control-danger mb-2 mr-sm-2 mb-sm-0');
     div.setAttribute('class', 'form-group has-danger');
     err.removeAttribute('hidden');
     err_mes.removeAttribute('hidden');
@@ -29,4 +29,45 @@ function register() {
     }
     request.open("POST", "/register");
     request.send(formData);
+}
+
+function skip() {
+    alert('/!\\ Il est conseiller de remplir ce formulaire affin de trouver les personnes qui vous correspondent \n- Si vous ne souhaiter pas remplir le formulaire tout de suite vous serez considerez comme Bisexuelle\n- Si vous avez deja entrer des tags ils seront quand meme enregistrer\n\nSouhaiter vous continuer sans remplir le formulaire?');
+}
+
+function add_tag() {
+    let tag = document.getElementById('add_tag');
+    let div = document.getElementById('add_tag_div');
+    let err = document.getElementById('add_tag_err');
+    let err_mes = document.getElementById('add_tag_err_mes');
+    let my_tag = document.getElementById('my_tag');
+    let my_tag_dis = document.getElementById('my_tag_dis');
+    let regex = /^#([a-z0-9]{1,20})$/;
+
+    if (regex.test(tag.value)) {
+        sucess(tag, div, err, err_mes);
+        if (my_tag_dis.hasAttribute('disabled')) {
+            my_tag.remove("Aucun centre d'interet");
+            my_tag_dis.removeAttribute('disabled');
+        }
+        let option = document.createElement("option");
+        option.text = tag.value;
+        my_tag.add(option);
+    }
+    else
+    danger(tag, div, err, err_mes);
+}
+
+function del_tag() {
+    let my_tag = document.getElementById('my_tag');
+    if (!my_tag_dis.hasAttribute('disabled')) {
+        my_tag.remove(my_tag.value);
+        if (my_tag.length == 0) {
+            document.getElementById('my_tag_dis').setAttribute('disabled', '');
+            let option = document.createElement("option");
+            option.text = "Aucun centre d'interet";
+            my_tag.add(option);
+        }
+
+    }
 }
